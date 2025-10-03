eac44426a111c8f42d4119391a328ee2faa58d25
leonDenizard <de.nizard@hotmail.com>
2025-10-03 10:16:51 -0300

    chore(security): add helmet, cors in app.ts for api protection

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express from "express";
+import helmet from "helmet";
+import cors from "cors";
 import visitsRoute from "./routes/visits.routes";
 import usersRoute from "./routes/users.routes";
 import swaggerConfig = require("./docs/swagger.config");
 
 const app = express();
+
 app.use(express.json());
+app.use(helmet());
+app.use(cors())
+
 app.use("/api/visits", visitsRoute);
 app.use("/api/users", usersRoute);
+
 swaggerConfig.setupSwagger(app);
 
 export default app;
